66731c2d5bfd79b22d0e3a7f93b798b1c4d92b67
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2023-10-31 22:17:45 +0530

    Update Promise.js

--- IP/promises/Promise.js
+++ IP/promises/Promise.js
@@ -26,3 +26,25 @@ const fetchApi = () => {
 
 prom = fetchApi()
 console.log(prom);
+
+// const axios = require('axios');
+
+// const fetchApi = () => {
+//   return new Promise((resolve, reject) => {
+//     axios.get("https://catfact.ninja/fact")
+//       .then((response) => {
+//         console.log(response.data);
+//         resolve(response.data);
+//       })
+//       .catch((error) => {
+//         console.error(`Data not fetched: ${error.message}`);
+//         reject(error);
+//       })
+//       .finally(() => {
+//         console.log("Fetch API executed");
+//       });
+//   });
+// };
+
+// const data = fetchApi()
+// console.log(data);
